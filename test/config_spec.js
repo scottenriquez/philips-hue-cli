e3c432dd219601146b4fc04c15aea1397b7a2c9a
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-05-29 20:12:36 -0500

    Added unit test outline

---
+++ test/config_spec.js
@@ -0,0 +1,11 @@
+describe('Using configuration service to', () => {
+    describe('obtain profile', () => {
+        it('should read the correct file from the file system');
+    });
+});
+
+describe('Using configuration service to', () => {
+    describe('create profile', () => {
+        it('should write the new profile to the file system');
+    });
+});
---
+++ test/hue_spec.js
@@ -0,0 +1,22 @@
+describe('Using the Hue service to', () => {
+    describe('initialize CLI', () => {
+        it('should check to see if a connection profile exists');
+        it('should create a new profile if one does not exist');
+    });
+});
+
+describe('Using the Hue service to', () => {
+    describe('turn on all lights', () => {
+        it('should read the local bridge connection profile');
+        it('should establish a connection with the bridge');
+        it('should turn on all the lights');
+    });
+});
+
+describe('Using the Hue service to', () => {
+    describe('turn on all lights', () => {
+        it('should read the local bridge connection profile');
+        it('should establish a connection with the bridge');
+        it('should turn off all the lights');
+    });
+});
